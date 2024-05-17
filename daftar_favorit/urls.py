f1bef9aa3d69e1a21d08e47b98c2f4a9af103a1e
lizidelyx <alizza.deli28@gmail.com>
2024-05-18 00:13:23 +0700

    perbaikan navbar

--- daftar_favorit/urls.py
+++ daftar_favorit/urls.py
@@ -5,7 +5,7 @@ app_name = 'daftar_favorit'
 
 urlpatterns = [
     path('', show_daftar_favorit, name='show_daftar_favorit'),
-  
+    path('show_data_favorit/', show_daftar_favorit, name='show_daftar_favorit'),
     path('hapus_favorit/', hapus_favorit, name='hapus_favorit')
     
 ]
