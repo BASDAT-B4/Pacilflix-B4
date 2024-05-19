c377bc8cdb9efa19d3f6ac1418ab5800a4ea5314
lizidelyx <alizza.deli28@gmail.com>
2024-05-19 23:48:43 +0700

    fitur hapus unduhan done

--- daftar_unduhan/query.py
+++ daftar_unduhan/query.py
@@ -12,14 +12,30 @@ from django.db import connection
 from django.db import connection
 
 
+# def get_daftar_unduhan(username):
+#     with connection.cursor() as cursor:
+#         cursor.execute("SELECT t.id_tayangan, t.username, t.timestamp, j.judul FROM pacilflix.tayangan_terunduh AS t JOIN pacilflix.tayangan AS j ON t.id_tayangan = j.id WHERE t.username = %s", [username])
+#         columns = [col[0] for col in cursor.description]
+#         return [
+#             dict(zip(columns, row))
+#             for row in cursor.fetchall()
+#         ]
+
+
+
+import datetime
+
 def get_daftar_unduhan(username):
     with connection.cursor() as cursor:
         cursor.execute("SELECT t.id_tayangan, t.username, t.timestamp, j.judul FROM pacilflix.tayangan_terunduh AS t JOIN pacilflix.tayangan AS j ON t.id_tayangan = j.id WHERE t.username = %s", [username])
         columns = [col[0] for col in cursor.description]
-        return [
-            dict(zip(columns, row))
-            for row in cursor.fetchall()
-        ]
-
-
-
+        unduhan_list = []
+        for row in cursor.fetchall():
+            unduhan = dict(zip(columns, row))
+            # Periksa jika timestamp kurang dari 1 hari
+            timestamp = unduhan.get('timestamp')
+            if timestamp:
+                kurang_dari_1_hari = (datetime.datetime.now() - timestamp).days < 1
+                unduhan['kurang_dari_1_hari'] = kurang_dari_1_hari
+            unduhan_list.append(unduhan)
+        return unduhan_list
